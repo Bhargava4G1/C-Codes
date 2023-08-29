7c5be2ceb0764c7be6e5310fdf63649ba05fa441
Bhargava Sai Voleti <31024472+Bhargava4G1@users.noreply.github.com>
2023-08-29 14:44:23 +0530

    Update and rename numbers to read_and_print_num.c

---
+++ read_and_print_num.c
@@ -0,0 +1,15 @@
+/*
+    Read two numbers and print the numbers
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n,n1;
+    printf("Enter number 1: ");
+    scanf("%d",&n);
+    printf("Enter number 2: ");
+    scanf("%d",&n1);
+    printf("Number 1 is : %d\nNumber 2 is : %d\n",n,n1);
+    return 0;
+}
