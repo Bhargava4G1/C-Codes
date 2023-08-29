e1fd4a29420e378b0ff6cf935bed9a1b63512ec5
Bhargava Sai Voleti <31024472+Bhargava4G1@users.noreply.github.com>
2023-08-29 14:56:40 +0530

    Update product_of_nums.c

--- numbers/product_of_nums.c
+++ numbers/product_of_nums.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2, sum=0;
+    int num1,num2, mul;
     printf("Enter number 1: ");
     scanf("%d",&num1);
     printf("Enter number 2: ");
     scanf("%d",&num2);
-    sum = num1 * num2;
-    printf("Product of %d and %d is %d\n",num1,num2, sum);
+    mul = num1 * num2;
+    printf("Product of %d and %d is %d\n",num1,num2, mul);
     return 0;
 }
