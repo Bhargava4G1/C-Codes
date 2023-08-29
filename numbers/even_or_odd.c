9aa25cf92006ab28b064c55a4da2dd58b025bad6
Bhargava Sai Voleti <31024472+Bhargava4G1@users.noreply.github.com>
2023-08-29 15:03:49 +0530

    Update even_or_odd.c

--- numbers/even_or_odd.c
+++ numbers/even_or_odd.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 int main()
 {
-    int num1;
-    printf("Enter number 1: ");
-    scanf("%d",&num1);
-    if(num1 % 2 == 0)
-        printf("%d is Even Number\n",num1);
+    int num;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    if(num % 2 == 0)
+        printf("%d is Even Number\n",num);
     else
-        printf("%d is Odd Number\n", num1);
+        printf("%d is Odd Number\n", num);
     return 0;
 }
