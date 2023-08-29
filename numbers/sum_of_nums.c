86059caa1fc6c65275d309540a2b4f19a1e29a2a
Bhargava Sai Voleti <31024472+Bhargava4G1@users.noreply.github.com>
2023-08-29 14:51:11 +0530

    Create sum_of_nums.c

---
+++ numbers/sum_of_nums.c
@@ -0,0 +1,16 @@
+/*
+    Read two numbers and perform addition
+*/
+
+#include <stdio.h>
+int main()
+{
+    int num1,num2, sum=0;
+    printf("Enter number 1: ");
+    scanf("%d",&num1);
+    printf("Enter number 2: ");
+    scanf("%d",&num2);
+    sum = num1 + num2;
+    printf("Sum of %d and %d is %d\n",num1,num2, sum);
+    return 0;
+}
