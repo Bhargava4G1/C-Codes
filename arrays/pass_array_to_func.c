867cbc66d3ce4d474cb5791d40fb757379391c27
Bharava4G1 <bhargavasaivoleti4@gmail.com>
2024-04-05 06:56:50 +0530

    Notes on 1Darray passing to function Example Code

---
+++ arrays/pass_array_to_func.c
@@ -0,0 +1,28 @@
+/*
+*   Program to understand the effect of passing an array to a function
+*/
+
+#include<stdio.h>
+void func(int val[]);
+int main()
+{
+    int i, arr[5]={1,2,3,4,5};
+    func(arr);
+    printf("Contents of array are : ");
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+void func(int val[])
+{
+    int sum = 0, i;
+    for(i = 0; i < 5; i++)
+    {
+        val[i] = val[i] * val[i];
+        sum += val[i];
+    }
+    printf("Sum of squares is %d\n",sum);
+}
