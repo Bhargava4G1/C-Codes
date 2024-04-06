a1547280a5734f394fa427cb19abfd45bb87a2c4
Bharava4G1 <bhargavasaivoleti4@gmail.com>
2024-04-07 01:15:52 +0530

    BITWISE SUBTRACTION

---
+++ c_practice_interview/bitwise/subtractio.c
@@ -0,0 +1,28 @@
+/*
+*   BITWISE SUBTRACTION
+*/
+
+#include<stdio.h>
+
+int bitWiseAddition(int n1, int n2)
+{
+    int carry = 0;
+    while (n2 != 0)
+    {
+        carry = (~n1) & n2;
+        n1 = n1 ^ n2;
+        n2 = carry << 1;
+    }
+    return n1;
+    
+}
+
+int main()
+{
+    int num1, num2;
+    printf("Enter num1 : ");
+    scanf("%d",&num1);
+    printf("Enter num2 : ");
+    scanf("%d",&num2);
+    printf("Sub of %d and %d is %d\n",num1, num2, bitWiseAddition(num1, num2));
+}
